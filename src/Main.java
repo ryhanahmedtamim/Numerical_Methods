f1f32413489144b54e28a575a489aa4095291ba8
ryhanahmedtamim <ryhanahmedtamim@gmail.com>
2018-02-10 14:48:06 +0600

    Complete Bisection Method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,72 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Function1 f = new Function1(68.1,40);
-		BisectionMethod b = new BisectionMethod(12,16,9,.5,f,14.7802);
-		b.getRoot();
+		print("For Roots of Equation : 1\n");
+		print("For Differention : 2\n");
+		print("For Integration : 3\n");
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int x;
+		x=sc.nextInt();
+		
+		if(x == 1)
+		{
+			
+			print("For Brackting Method : 1\n");
+			print("For Open Method : 2\n");
+			
+			int xx = sc.nextInt();
+			
+			if(xx == 1)
+			{
+				print("Enter the value of Mass :");
+				double m = sc.nextDouble();
+				print("Enter the value of Velocity :");
+				double v = sc.nextDouble();
+				Function1 f = new Function1(m,v);
+				
+				print("Enter the value of lower guess xl :");
+				double xl = sc.nextDouble();
+				print("Enter the value of upper guess xu :");
+				double xu = sc.nextDouble();
+				print("Enter the value of Maxmum iteration :");
+				int it = sc.nextInt();
+				print("Enter the True value :");
+				double tr = sc.nextDouble();
+				print("Enter the value maximum error:");
+				double es = sc.nextDouble();
+				
+				BisectionMethod b = new BisectionMethod(xl,xu,it,es,f,tr);
+				b.getRoot();
+			}
+			else if(xx == 2)
+			{
+				
+				
+			}
+			else
+			{
+				print("Wrong selection\n");
+			}
+			
+		}
+		else 
+		{
+			
+		}
+		
+		
+		
+		
 
+	}
+	
+	public static void print(Object o)
+	{
+		System.out.print(o);
 	}
 
 }
